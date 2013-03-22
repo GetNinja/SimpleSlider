cad71a0733b60ccf76f506c01fef9d837228a17a
Luke Scalf <luke.scalf@izoox.com>
2013-03-21 19:04:23 -0600

    Refactoring code to allow for starting/pausing/resuming of slideshows from outside plugin.

--- js/jquery.simpleslider.js
+++ js/jquery.simpleslider.js
@@ -1,213 +1,217 @@
  /**
- * jQuery SimpleSlider Plugin
- * This jQuery plugin was created by Luke Scalf at MonkeyWrench (https://github.com/monkey-wrench/SimpleSlider)
- * @name jquery.simpleslider.js
- * @author Luke Scalf - http://www.monkeywrench.cc/
- * @version 0.2
- * @date January 8, 2012
- * @category jQuery plugin
- * @copyright (c) 2012-2013 MonkeyWrench (monkeywrench.cc)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+  * jQuery SimpleSlider Plugin
+  * This jQuery plugin was created by Luke Scalf at MonkeyWrench (https://github.com/GetNinja/SimpleSlider)
+  * @name jquery.simpleslider.js
+  * @author Luke Scalf - http://www.monkeywrench.cc/
+  * @version 1.0.0
+  * @date March 21, 2013
+  * @category jQuery plugin
+  * @copyright (c) 2012-2013 MonkeyWrench (monkeywrench.cc)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 
-(function($)
-{
+;(function($) {
     /**
-     * SimpleSlider: Initialize Plugin
+     * SimpleSlider: Object Instance
      */
-    $.fn.simpleSlider = function(options)
-    {
-        var options = $.extend({}, $.fn.simpleSlider.defaults, options);
+    $.simpleSlider = function(el, options) {
+        var slider  = $(el),
+            options = $.extend({}, $.simpleSlider.defaults, options),
+            newImage;
 
-        return this.each(function()
-        {
-            // Variables used for storing state information about each slider
-            var slider           = $(this);
-            var current_image    = options.start;
-            var image_count      = $(slider).children('.images').children().length;
-            var time_out         = null;
+        // Store a reference to the slider object
+        $.data(el, 'simpleSlider', slider);
 
-            // Make sure starting image id is less than total image count and greater than 0
-            if (current_image > image_count || current_image < 1) {
-                current_image = 1;
-            }
+        // Private Methods
+        methods = {
+            init: function() {
+                slider.currentImage = options.start,
+                slider.imageCount   = slider.children('.images').children().length,
+                slider.nextImage    = slider.currentImage;
 
-            // Set display: 'none' for all slides except starting slide
-            $('.images > *', slider).css({display: 'none'});
-            $('.images .' + options.slide_class + current_image, slider).css({display: 'inline'});
-
-            // Generate navigation, if enabled
-            if (options.navigation == true) {
-                generate_navigation();
-            }
-
-            // Initialize Slideshow
-            time_out = setTimeout(play, options.delay);
-
-            /**
-             * SimpleSlider Function (play): Starts the slider
-             */
-            function play()
-            {
-                next_image = current_image;
-
-                if (next_image == image_count) {
-                    next_image = 0;
+                // Make Sure starting image id is less than total image count and greater than 0)
+                if (slider.currentImage > slider.imageCount || slider.currentImage < 1) {
+                    slider.currentImage = 1;
                 }
-                next_image++;
-                
-                goto_image(next_image);
-                
-                time_out = setTimeout(play, options.delay);
-            }
 
-            /**
-             * SimpleSlider Function (pause): Pauses the timer on mouse over
-             */
-            function pause() {
-                clearTimeout(time_out);
-            }
+                // Set 'display: none' for all slides except starting slide
+                $('.images > *', slider).css({ display: 'none' });
+                $('.images .' + options.slideClass + slider.currentImage, slider).css({ display: 'inline' });
 
-            /**
-             * SimpleSlider Function (generate_navigation): Generates Slide Navigation
-             */
-            function generate_navigation()
-            {
+                // Generate navigation, if enabled
+                if (options.navigation) {
+                    methods.generateNavigation();
+                }
+
+                // Start Slideshow
+                slider.timer = setTimeout(slider.play, options.delay);
+
+                // Event Handlers: Handles click of slider navigation, if enabled
+                if(options.navigation == true) {
+                    $('.' + options.navContainer + ' li a', slider.parent()).bind('click', methods.navigationHandler);
+                }
+            },
+            generateNavigation: function() {
                 var navigation = '';
-                for (i = 1; i <= image_count; i++) {
-                    navigation += '<li id="image' + i + '"><a href="#">&nbsp;</a></li>';
+                for (i = 1; i <= slider.imageCount; i++) {
+                    navigation += '<li id="'+options.slideClass+i+'"><a href="#">&nbsp;</a></li>';
                 }
-                $('.' + options.nav_container).html(navigation);
-                $('#' + options.slide_class + current_image).addClass('active');
-            }
-
-            /**
-             * SimpleSlider Function (update_navigation): Updates Slide Navigation
-             */
-            function update_navigation(new_image)
+                $('.' + options.navContainer, slider.parent()).html(navigation);
+                $('#' + options.slideClass + slider.currentImage, slider.parent()).addClass('active');
+            },
+            updateNavigation: function(imageId)
             {
-                $('.' + options.nav_container + ' li').removeClass('active');
-                $('#' + options.slide_class + new_image).addClass('active');
-            }
+                $('.' + options.navContainer + ' li', slider.parent()).removeClass('active');
+                $('#' + options.slideClass + imageId).addClass('active');
+            },
+            navigationHandler: function(e) {
+                e.preventDefault();
 
-            /**
-             * SimpleSlider Function (goto_image): Animation handler
-             */
-            function goto_image(image_id)
+                newImage = $(this).parent().attr('id').replace('image', '');
+                methods.gotoImageAndRestart(newImage);
+            },
+            gotoImage: function(imageId)
             {
-                if(!image_id) {
-                	return true;
-                }
+                if(!imageId) return true;
+
+                //alert(imageId);
 
                 // Update navigation, if enabled
                 if(options.navigation == true) {
-                    update_navigation(image_id);
+                    methods.updateNavigation(imageId);
                 }
 
                 switch (options.effect) {
                     case 'fade-in':
-                        $('.images .' + options.slide_class + image_id, slider).css({
+                        $('.images .' + options.slideClass + imageId, slider).css({
                             display: 'none',
                             zIndex: 2
                         }).fadeIn(options.transition, function()
                         {
-                            $('.images .' + options.slide_class + current_image, slider).css({display: 'none'});
-                            $('.images .' + options.slide_class + image_id, slider).css({zIndex: 1});
-                            current_image = image_id;
+                            $('.images .' + options.slideClass + slider.currentImage, slider).css({display: 'none'});
+                            $('.images .' + options.slideClass + imageId, slider).css({zIndex: 1});
+                            slider.currentImage = imageId;
                         });
                         break;
                     case 'fade-out':
-                        $('.images .' + options.slide_class + current_image, slider).css({zIndex: 2});
-                        $('.images .' + options.slide_class + image_id, slider).css({display: 'inline', zIndex: 1});
-                        $('.images .' + options.slide_class + current_image, slider).fadeOut(options.transition, function()
+                        $('.images .' + options.slideClass + slider.currentImage, slider).css({zIndex: 2});
+                        $('.images .' + options.slideClass + imageId, slider).css({display: 'inline', zIndex: 1});
+                        $('.images .' + options.slideClass + slider.currentImage, slider).fadeOut(options.transition, function()
                         {
-                            current_image = image_id;
+                            slider.currentImage = imageId;
                         });
                         break;
                     case 'cross-fade':
-                        $('.images .' + options.slide_class + current_image, slider).css({zIndex: 2});
-                        $('.images .' + options.slide_class + image_id, slider).css({display: 'inline', zIndex: 1});
-                        $('.images .' + options.slide_class + image_id, slider).fadeIn(options.transition);
-                        $('.images .' + options.slide_class + current_image, slider).fadeOut(options.transition, function()
+                        $('.images .' + options.slideClass + slider.currentImage, slider).css({zIndex: 2});
+                        $('.images .' + options.slideClass + imageId, slider).css({display: 'inline', zIndex: 1});
+                        $('.images .' + options.slideClass + imageId, slider).fadeIn(options.transition);
+                        $('.images .' + options.slideClass + slider.currentImage, slider).fadeOut(options.transition, function()
                         {
-                            current_image = image_id;
+                            slider.currentImage = imageId;
                         });
                         break;
                     case 'no-effect':
                     default:
-                        $('.images .' + options.slide_class + current_image, slider).hide();
-                        $('.images .' + options.slide_class + image_id, slider).show();
-                        current_image = image_id;
+                        $('.images .' + options.slideClass + slider.currentImage, slider).hide();
+                        $('.images .' + options.slideClass + imageId, slider).show();
+                        slider.currentImage = imageId;
                         break;
                 }
-            }
-
-            /**
-             * SimpleSlider Function (goto_image_restart): Goes to specified image, and restarts animation
-             */
-            function goto_image_restart(image_id)
+            },
+            gotoImageAndRestart: function(imageId)
             {
-                if(!image_id) {
+                if(!imageId) {
                     return true;
                 }
-                
-                pause();
-                goto_image(image_id);
-                time_out = setTimeout(play, options.delay);
-            }
 
-            /**
-             * Event Handlers: Handles pausing on slider hover, if enabled
-             */
-            if(options.pause == true) {
-                $('.images', slider).mouseover(function(e)
-                {
-                    pause();
-                });
-                $('.images', slider).mouseleave(function(e)
-                {
-                    play();
-                });
+                slider.pause();
+                methods.gotoImage(imageId);
+                slider.timer = setTimeout(slider.play, options.delay);
             }
+        };
 
-            /**
-             * Event Handlers: Handles click of slider navigation, if enabled
-             */
-            if(options.navigation == true) {
-                $('.' + options.nav_container + ' li a').click(function(e)
-                {
-                    e.preventDefault();
-                    
-                    var new_image = $(this).parent().attr('id').replace('image', '');
-                    goto_image_restart(new_image);
-                });
+        // Public functions
+        slider.play = function() {
+            slider.nextImage = slider.currentImage;
+            if (slider.nextImage == slider.imageCount) {
+                slider.nextImage = 0;
             }
-        });
+            slider.nextImage++;
+
+            methods.gotoImage(slider.nextImage);
+
+            slider.timer = setTimeout(slider.play, options.delay);
+        }
+
+        slider.pause = function() {
+            clearTimeout(slider.timer);
+            $('.'+options.navContainer, slider.parent()).fadeOut();
+        }
+
+        slider.resume = function() {
+            $('.'+options.navContainer, slider.parent()).fadeIn();
+            slider.play();
+        }
+
+        // Initialize the slider
+        methods.init();
+    }
+
+    /**
+     * SimpleSlider: Plugin Function
+     */
+    $.fn.simpleSlider = function(options) {
+        if (options === undefined) options = {};
+
+        if (typeof options === 'object') {
+            return this.each(function() {
+                var $this    = $(this),
+                    selector = '.images > *',
+                    $slides  = $this.find(selector);
+
+                if ($slides.length > 1) {
+                    new $.simpleSlider(this, options);
+                }
+            });
+        } else {
+            // Helper strings to quickly perform functions on the slider
+            var $slider = $(this).data('simpleSlider');
+            switch (options) {
+                case 'play':
+                    $slider.play();
+                    break;
+                case 'resume':
+                    $slider.resume();
+                    break;
+                case 'pause':
+                    $slider.pause();
+                    break;
+            }
+        }
     };
 
     /**
      * SimpleSlider: Default Options
      */
-    $.fn.simpleSlider.defaults = {
-        container: 'simple-slider',               // String, Class name for gallery container. Default is 'simple-slider'
-        delay: 0,                                 // Number, Determines time in milliseconds between slide transitions, Default 0
-        effect: 'fade-out',                       // String, Type of effect to use, Options are 'fade-in', 'fade-out', 'cross-fade', 'no-effect'
-        navigation: false,                        // Boolean, Determines whether or not to have slide navigation circles, default false
-        nav_container: 'slider-navigation',       // String, Class name for navigation container, default 'slider-navigation'
-        pause: true,                              // Boolean, Determines whether or not to pause the slider on mouse over, Default true
-        slide_class: 'image',                     // String, Class name for slides, do not include the slide number here. Default 'image'
-        start: 1,                                 // Number, Determines which slide to start with, Default 1
-        transition: 500,                          // Number, Determines time in milliseconds the slide transitions will take, Default 500
+    $.simpleSlider.defaults = {
+        container:    'simple-slider',       // String, Class name for gallery container. Default is 'simple-slider'
+        delay:        0,                     // Number, Determines time in milliseconds between slide transitions, Default 0
+        effect:       'fade-out',            // String, Type of effect to use, Options are 'fade-in', 'fade-out', 'cross-fade', 'no-effect'
+        navigation:   false,                 // Boolean, Determines whether or not to have slide navigation circles, default false
+        navContainer: 'slider-navigation',   // String, Class name for navigation container, default 'slider-navigation'
+        slideClass:   'image',               // String, Class name for slides, do not include the slide number here. Default 'image'
+        start: 1,                            // Number, Determines which slide to start with, Default 1
+        transition: 500,                     // Number, Determines time in milliseconds the slide transitions will take, Default 500
     };
 })(jQuery);
